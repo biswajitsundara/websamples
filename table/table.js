0d1590b0f476fce04bc5a89e84d49c3c57bf4c1c
Biswajit Sundara <biswajitsundara@gmail.com>
2023-04-29 21:49:02 +0530

    table updates

--- table/table.js
+++ table/table.js
@@ -1,20 +1,29 @@
+let refreshCount = 0;
+const maxRefreshCount = 5;
+let jobID;
+
 function addJob() {
   var newRow = document.createElement("tr");
-  var jobID = document.getElementById("jobTable").rows.length;
+  jobID = document.getElementById("jobTable").rows.length;
   var jobName = "Create a New User job " + jobID;
   var jobStatus = "Pending";
 
   newRow.innerHTML = `<td>${jobID}</td>
       <td>${jobName}</td>
-      <td id=job${jobID}Status>${jobStatus}</td>
+      <td id=job${jobID}Status class='pending'>${jobStatus}</td>
      `;
 
   document.getElementById("jobTable").appendChild(newRow);
+}
 
-  setTimeout(function () {
-    const cellId = `job${jobID}Status`;
-    document.getElementById(cellId).innerHTML = "Success";
-    document.getElementById(cellId).classList.add('success');
-
-  }, 5000);
+function refreshStatus() {
+  refreshCount++;
+  if (refreshCount >= maxRefreshCount) {
+    setTimeout(function () {
+      const cellId = `job${jobID}Status`;
+      document.getElementById(cellId).innerHTML = "Success";
+      document.getElementById(cellId).classList.remove("pending");
+      document.getElementById(cellId).classList.add("success");
+    }, 5000);
+  }
 }
